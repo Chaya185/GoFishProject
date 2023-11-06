b9fe22f586e0ce2445096a2b29e41b7857131f1b
Chayki Kanevski <126410874+Chaya185@users.noreply.github.com>
2023-11-05 20:15:08 -0500

    Update ImplementCard.java

--- ImplementCard.java
+++ ImplementCard.java
@@ -1,14 +1,17 @@
 // Coded by Chaya Kanevski
 package GoFishProject;
 public class ImplementCard implements Card {
+    //fields
     private String suit;
     private String rank;
 
+    //constructor that initializes rand and suit of a card
     public ImplementCard(String rank, String suit) {
         this.rank = rank;
         this.suit = suit;
     }
 
+    //getters
     public String getRank(){
         return this.rank;
     }
@@ -17,10 +20,10 @@ public class ImplementCard implements Card {
         return this.suit;
     }
 
+    //toString method for display
     public String toString(){
         StringBuilder sb = new StringBuilder();
         sb.append(rank);
         return sb.toString();
     }
-
 }
