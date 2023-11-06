d820ed677a0b0425f387d7027176ca6b16ab0435
Chayki Kanevski <126410874+Chaya185@users.noreply.github.com>
2023-11-05 20:19:58 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,13 +3,14 @@ package GoFishProject;
 import java.util.List;
 
 public interface Player {
-	String getName();
-    List<Card> getHand();
+	//player fields
+    	String getName();
+    	List<Card> getHand();
 
-    int getSets();
-    void setSets(int sets);
-    void addToHand(Card card);
-    Card play();
+	int getSets();
+    	void setSets(int sets);
+    	void addToHand(Card card);
+    	Card play();
 	void removeFromHand(Card card);
 	int checkForSets();
 
