83fc711f030f6902fea7af452e3314bb1ffea756
Chayki Kanevski <126410874+Chaya185@users.noreply.github.com>
2023-11-05 20:14:01 -0500

    added comments

--- HumanPlayer.java
+++ HumanPlayer.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class HumanPlayer implements Player {
+    //human player fields
     private String name;
     private List<Card> hand;
     private int hSetCounter;
@@ -11,6 +12,7 @@ public class HumanPlayer implements Player {
 
     Scanner scanner = new Scanner(System.in);
 
+    //constructor 
     public HumanPlayer(String name, List<Card> hand, int sets) {
         this.name = name;
         this.hand = hand;
@@ -19,6 +21,7 @@ public class HumanPlayer implements Player {
     }
 
 
+    //getters and setter methods
     @Override
     public String getName() {
         return name;
@@ -53,27 +56,28 @@ public class HumanPlayer implements Player {
 
     //until here coded by Chaya, play() coded by Shana
     @Override
+    //this method handles the user's logic every turn, gets their input and displays
     public Card play() {
         if (hand.isEmpty()) {
             System.out.println(getName() + ", you have no cards to play.");
-
         }
 
-        // Print the player's hand
+        // Print the player's hand so they see what cards to choose from
         System.out.println(getName() + ", your current hand:");
         for (int i = 0; i < hand.size(); i++) {
             System.out.println((i + 1) + ". " + hand.get(i));
         }
 
+        //ask the user which of their cards they want to play
         System.out.print("Enter the number of the card you want to play (1-" + hand.size() + "): ");
         int selectedCardIndex = scanner.nextInt() - 1;
         
-       
+
+        //input validation to make sure the user chose a card from their hand
         while (selectedCardIndex < 0 || selectedCardIndex >= hand.size()) {
             System.out.println("Invalid card selection. Please select a valid card.");
             System.out.print("Enter the number of the card you want to play (1-" + hand.size() + "): ");
             selectedCardIndex = scanner.nextInt() - 1;
-
         }
 
         Card cardToPlay = hand.get(selectedCardIndex);
@@ -91,6 +95,7 @@ public class HumanPlayer implements Player {
         while (i < hand.size()) {
             Card card = hand.get(i);
             int count = 0;
+            //loop through the opponents hand and check for present requested card
             for (int j = 0; j < hand.size(); j++) {
                 if (hand.get(j).getRank().equals(card.getRank())) {
                     count++;
